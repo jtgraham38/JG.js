3c254525b2058ec0c2c8651e78d155c7f07b8dc3
Jacob <jtgraham38@gmail.com>
2024-03-07 14:03:41 -0500

    Implemented jg_ajax_get form submission functionality

---
+++ js/jg_ajax_form.js
@@ -0,0 +1,58 @@
+//define constants
+const JG_AJAX_GET_FORM_METHOD = 'jg_ajax_get' //method used for forms that should be via ajax get
+const JG_AJAX_POST_FORM_METHOD = 'jg_ajax_get' //method used for forms that should be via ajax post
+const JG_AJAX_RESPONSE_HANDLER_ATTRNAME = 'jg_ajax_response_handler' //attribute used to specify a function to handle the response from an ajax request
+
+//animation used by the default loader
+
+//call functions
+document.addEventListener('DOMContentLoaded', (e)=>{
+    //apply ajax form submission to forms
+    jg_apply_ajax_get()
+    jg_apply_ajax_post()
+})
+
+/*
+This function finds all forms with the method in JG_AJAX_GET_FORM_METHOD and adds an event listener to submit the form via an ajax get request.
+*/
+function jg_apply_ajax_get(){
+    //get forms
+    let forms = Array.from(document.querySelectorAll(`form[method=${JG_AJAX_GET_FORM_METHOD}]`))
+
+    //for each form...
+    forms.map((form)=>{
+      //add onsubmit event listener to form to submit form via ajax
+        form.addEventListener('submit', async (event)=>{
+            event.preventDefault()
+
+            //get formdata to submit
+            let form_data = new FormData(event.target)
+            const form_obj = {};
+            form_data.forEach((value, key) => {
+                form_obj[key] = value;
+            });
+            console.log(form_obj)
+
+            const params = new URLSearchParams(form_data).toString()
+
+            //get form action
+            const form_action = form.getAttribute('action');
+
+            //submit form data via ajax get request
+            const response = await fetch(form_action + "?" + params, {
+                method: 'GET'
+            })
+            const data = await response.json()
+
+            //call response handler function
+            const handler = form.getAttribute(JG_AJAX_RESPONSE_HANDLER_ATTRNAME)
+            if (handler && typeof window[handler] === 'function') eval(handler)(event, data)
+        });
+    });
+}
+/*
+This function finds all forms with the method in JG_AJAX_POST_FORM_METHOD and adds an event listener to submit the form via an ajax post request.
+*/
+function jg_apply_ajax_post(){
+    //TODO: implement
+}
