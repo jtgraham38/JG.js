7d7bec843bfcf8467965018bff259055334692d2
Jacob <jtgraham38@gmail.com>
2024-03-07 22:17:44 -0500

    implemented jg ajax post

--- js/jg_ajax_form.js
+++ js/jg_ajax_form.js
@@ -1,6 +1,6 @@
 //define constants
 const JG_AJAX_GET_FORM_METHOD = 'jg_ajax_get' //method used for forms that should be via ajax get
-const JG_AJAX_POST_FORM_METHOD = 'jg_ajax_get' //method used for forms that should be via ajax post
+const JG_AJAX_POST_FORM_METHOD = 'jg_ajax_post' //method used for forms that should be via ajax post
 const JG_AJAX_RESPONSE_HANDLER_ATTRNAME = 'jg_ajax_response_handler' //attribute used to specify a function to handle the response from an ajax request
 
 //animation used by the default loader
@@ -31,7 +31,6 @@ function jg_apply_ajax_get(){
             form_data.forEach((value, key) => {
                 form_obj[key] = value;
             });
-            console.log(form_obj)
 
             const params = new URLSearchParams(form_data).toString()
 
@@ -45,8 +44,8 @@ function jg_apply_ajax_get(){
             const data = await response.json()
 
             //call response handler function
-            const handler = form.getAttribute(JG_AJAX_RESPONSE_HANDLER_ATTRNAME)
-            if (handler && typeof window[handler] === 'function') eval(handler)(event, data)
+            const handler = eval(form.getAttribute(JG_AJAX_RESPONSE_HANDLER_ATTRNAME))
+            if (handler && typeof handler === 'function') handler(event, data)
         });
     });
 }
@@ -54,5 +53,38 @@ function jg_apply_ajax_get(){
 This function finds all forms with the method in JG_AJAX_POST_FORM_METHOD and adds an event listener to submit the form via an ajax post request.
 */
 function jg_apply_ajax_post(){
-    //TODO: implement
+    
+    //get forms
+    let forms = Array.from(document.querySelectorAll(`form[method=${JG_AJAX_POST_FORM_METHOD}]`))
+
+    //for each form...
+    forms.map((form)=>{
+      //add onsubmit event listener to form to submit form via ajax
+        form.addEventListener('submit', async (event)=>{
+            event.preventDefault()
+
+            //get formdata to submit
+            let form_data = new FormData(event.target)
+            const form_obj = {};
+            form_data.forEach((value, key) => {
+                form_obj[key] = value;
+            });
+
+            //get form action
+            const form_action = form.getAttribute('action');
+
+            //submit form data via ajax post request
+            const response = await fetch(form_action, {
+                method: 'POST',
+                body: JSON.stringify(form_obj)
+            })
+            const data = await response.json()
+
+            //call response handler function
+            const handler = eval(form.getAttribute(JG_AJAX_RESPONSE_HANDLER_ATTRNAME))
+            if (handler && typeof handler === 'function') handler(event, data)
+        });
+    });
 }
+
+
